f2f4599ac517d2ed05a4f8b7efcc8d09ef5c496e
nhorton06 <nathaniel.horton06@gmail.com>
2026-01-15 19:58:39 +0000

    Initial push

---
+++ python.py
@@ -0,0 +1,40 @@
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+# "Path": @nhorton06 ➜ /workspaces/daytwostuff (main)
+
+# The environment should not be included in the environment in the repo itself because it is a 
+# local environment specific to the user's machine.
+
+# New "path": (.venv) @nhorton06 ➜ /workspaces/daytwostuff (main)
+# The main difference is the (.venv) at the beginning, indcating the active virtual environment
+
+#%%
+import pandas as pd
+data = pd.read_csv('https://raw.githubusercontent.com/MMiDS-textbook/MMiDS-textbook.github.io/refs/heads/main/utils/datasets/penguins-measurements.csv')
+print(data.head())
+
+
+# %%
+
+# Extension menu observations: There is an icon showing
+# that the extension is installed and active in the codespace. 
+# There also is no longer an install button for the extension
+# as it is already installed.
+
+# 3 Useful elements of Data Wrangler extension:
+# 1. Instant transformations to data (fill, drop, type, etc.)
+# 2. Automatic pandas code preview and export
+# 3. Launch from many file types (csv, excel, json, etc.)
+
+# Plotly Version: 6.5.1
+
+# Why we use requirements.txt file:
+# To specify the exact versions of packages needed, ensure consistency across
+# users and environments, and make it easier to install needed dependencies.
+
